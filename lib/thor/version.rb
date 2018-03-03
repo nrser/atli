83c9a78b23eedca2d586c67290980eb4665486c9
nrser <neil@ztkae.com>
2018-03-03 19:20:54 +0800

    start 0.1.6.dev

--- lib/thor/version.rb
+++ lib/thor/version.rb
@@ -14,7 +14,7 @@ class Thor
   # 
   # @return [String]
   # 
-  VERSION = '0.1.5'
+  VERSION = '0.1.6.dev'
   
   
   # The version of Thor that Atli is up to date with.
@@ -27,5 +27,5 @@ class Thor
   # 
   # @return [String]
   # 
-  THOR_VERSION = '0.1.5'
+  THOR_VERSION = '0.1.6.dev'
 end
