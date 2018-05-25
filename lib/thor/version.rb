825672e9c914a25a0e42f564c456eaf350d588c6
nrser <neil@ztkae.com>
2018-05-26 04:05:05 +0800

    bump to v0.1.8

--- lib/thor/version.rb
+++ lib/thor/version.rb
@@ -14,7 +14,7 @@ class Thor
   # 
   # @return [String]
   # 
-  VERSION = '0.1.8.dev'
+  VERSION = '0.1.8'
   
   
   # The version of Thor that Atli is up to date with.
@@ -27,5 +27,5 @@ class Thor
   # 
   # @return [String]
   # 
-  THOR_VERSION = '0.1.8.dev'
+  THOR_VERSION = '0.1.8'
 end
