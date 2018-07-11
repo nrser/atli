e36a0226ba0865e056daa28926f51758daeb159a
nrser <neil@ztkae.com>
2018-07-12 05:21:04 +0800

    Requie {NRSER::RSpex} in spec helper, enable `focus:` config opt

--- spec/helper.rb
+++ spec/helper.rb
@@ -22,6 +22,9 @@ require "rspec"
 require "diff/lcs" # You need diff/lcs installed to run specs (but not to run Thor).
 require "webmock/rspec"
 
+require 'nrser'
+require 'nrser/rspex'
+
 WebMock.disable_net_connect!(:allow => "coveralls.io")
 
 # Set shell to basic
@@ -43,6 +46,13 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = \
     (Thor::ROOT / 'tmp' / '.rspec_status').to_s
+
+  # This allows you to limit a spec run to individual examples or groups
+  # you care about by tagging them with `:focus` metadata. When nothing
+  # is tagged with `:focus`, all examples get run. RSpec also provides
+  # aliases for `it`, `describe`, and `context` that include `:focus`
+  # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
+  config.filter_run_when_matching :focus
   
   config.before do
     ARGV.replace []
