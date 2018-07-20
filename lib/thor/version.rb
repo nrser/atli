73d5e4bd694f90115658cb62e7f0919bd8c0c345
nrser <neil@ztkae.com>
2018-07-20 09:21:23 +0800

    bump to v0.1.12

--- lib/thor/version.rb
+++ lib/thor/version.rb
@@ -14,7 +14,7 @@ class Thor
   # 
   # @return [String]
   # 
-  VERSION = '0.1.12.dev'
+  VERSION = '0.1.12'
   
   
   # The version of Thor that Atli is up to date with.
@@ -27,7 +27,7 @@ class Thor
   # 
   # @return [String]
   # 
-  THOR_VERSION = '0.1.12.dev'
+  THOR_VERSION = '0.1.12'
 
 
   # Are we running from the source code (vesus from a Gem install)?
