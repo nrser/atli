904c7667026c523a8112cfad5a2bc7d9bfb8136d
nrser <neil@ztkae.com>
2018-07-20 09:10:57 +0800

    start 0.1.12.dev

--- lib/thor/version.rb
+++ lib/thor/version.rb
@@ -14,7 +14,7 @@ class Thor
   # 
   # @return [String]
   # 
-  VERSION = '0.1.11'
+  VERSION = '0.1.12.dev'
   
   
   # The version of Thor that Atli is up to date with.
@@ -27,7 +27,7 @@ class Thor
   # 
   # @return [String]
   # 
-  THOR_VERSION = '0.1.11'
+  THOR_VERSION = '0.1.12.dev'
 
 
   # Are we running from the source code (vesus from a Gem install)?
